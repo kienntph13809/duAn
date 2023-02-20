1941918812584fe408432081299fc1df365aff5b
kiencoine <kien50626@gmail.com>
2023-02-20 15:21:23 +0700

    up code entity

---
+++ Childrenwear/src/main/java/com/example/childrenwear/ChildrenwearApplication.java
@@ -0,0 +1,13 @@
+package com.example.childrenwear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildrenwearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildrenwearApplication.class, args);
+    }
+
+}
---
+++ Childrenwear/src/test/java/com/example/childrenwear/ChildrenwearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.childrenwear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildrenwearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
